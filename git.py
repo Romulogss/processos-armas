f514edad6a6e5b6cec3da445ef1a988b984c9052
Romulogss <romulo.gabss@gmail.com>
2020-04-15 13:47:53 -0300

    CONCERTANDO ONBLUR DE REPLICAR DADOS, NA CERTIDÃO CRAF/PAF

--- git.py
+++ git.py
@@ -1,10 +1,10 @@
 import subprocess as cmd
 from sys import argv
 entrada = argv
-cmd.run('git add .', check=True, shell=True)
-cmd.run(f'git commit -m "{entrada[1]}"', check=True, shell=True)
 if entrada[2] == 'h':
     cmd.run('git push heroku master', check=True, shell=True)
 else:
+    cmd.run('git add .', check=True, shell=True)
+    cmd.run(f'git commit -m "{entrada[1]}"', check=True, shell=True)
     cmd.run('git push origin master', check=True, shell=True)
 cmd.run("echo 'repositório atualizado com sucesso!'", check=True, shell=True)    
