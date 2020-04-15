82c2ca4e7845584f7930125cab36737dc3a9ecd2
Romulogss <romulo.gabss@gmail.com>
2020-04-15 18:25:43 -0300

    testando git.py

--- git.py
+++ git.py
@@ -1,8 +1,15 @@
 import subprocess as cmd
 from sys import argv
 entrada = argv
-if entrada[2] == 'h':
-    cmd.run('git push heroku master', check=True, shell=True)
+if len(entrada) == 3:
+    try:
+        cmd.run('git add .', check=True, shell=True)
+        cmd.run(f'git commit -m "{entrada[1]}"', check=True, shell=True)
+        cmd.run('git push origin master', check=True, shell=True)
+        cmd.run('git push heroku master', check=True, shell=True)
+    except Exception as e:
+        cmd.run('git push heroku master', check=True, shell=True)
+
 else:
     cmd.run('git add .', check=True, shell=True)
     cmd.run(f'git commit -m "{entrada[1]}"', check=True, shell=True)
