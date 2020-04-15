c5c7e23af6107f1285e3db31b4ea6383cb25432e
Romulogss <romulo.gabss@gmail.com>
2020-04-15 11:53:56 -0300

    arrumando scrpit de commit

--- git.py
+++ git.py
@@ -2,9 +2,9 @@ import subprocess as cmd
 from sys import argv
 entrada = argv
 cmd.run('git add .', check=True, shell=True)
-cmd.run(f'git commit -m "{entrada[1]}"')
+cmd.run(f'git commit -m "{entrada[1]}"', check=True, shell=True)
 if entrada[2] == 'h':
-    cmd.run('git push heroku master')
+    cmd.run('git push heroku master', check=True, shell=True)
 else:
-    cmd.run('git push origin master')
-cmd.run("echo 'repositório atualizado com sucesso!'")    
+    cmd.run('git push origin master', check=True, shell=True)
+cmd.run("echo 'repositório atualizado com sucesso!'", check=True, shell=True)    
