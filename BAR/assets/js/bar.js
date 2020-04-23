a3690176c531a3f256397876fda6f923591a3506
Romulogss <romulo.gabss@gmail.com>
2020-04-23 16:03:11 -0300

    arrumando of

--- BAR/assets/js/bar.js
+++ BAR/assets/js/bar.js
@@ -243,6 +243,14 @@ const pegarCPF = processo => {
 
 }
 
+const mCPF = (cpf) => {
+    cpf=cpf.replace(/\D/g,"")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return cpf
+}
+
 const pegarData = (id, processo) => {
     let data = pegar(id, processo);
     data = data.split('-');
